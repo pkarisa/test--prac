8e9700d3c2f7632e18a2e402469783b259284811
pkarisa <faith.pendo@student.moringaschool.com>
2023-06-21 14:59:07 +0300

    test ammended

--- new.js
+++ new.js
@@ -1,3 +1,25 @@
-var myNumber = 3;
-console.log ("myNumber");
+function longestConsecutiveSubsequenceLength(N) {
+    const numSet = new Set(N);
+    let maxLength = 0;
+  
+    for (const num of N) {
+      if (!numSet.has(num - 1)) {
+        let length = 1;
+        let currentNum = num + 1;
+  
+        while (numSet.has(currentNum)) {
+          length++;
+          currentNum++;
+        }
+  
+        maxLength = Math.max(maxLength, length);
+      }
+    }
+  
+    return maxLength;
+  }
+  const N = [10, 4, 20, 1, 3, 2];
+const result = longestConsecutiveSubsequenceLength(N);
+console.log(result);  // answer is 4
+  
 
--- test.js
+++ test.js
@@ -1,7 +1,103 @@
-const para = document.querySelector("p");
-para.addEventListener("click",updateName);
+import cx from "classnames";
 
-function updateName() {
-    const name = pompt(" Entername");
-    para.textContent = 'Player 1: ${name}'
+
+
+
+import { Component } from "react";
+
+
+
+
+export default class TodoList extends Component {
+
+     changeclass(e) {
+
+        if (document.getElementById(e).classList.contains('is-done')) {
+
+          document.getElementById(e).classList.remove('is-done');
+
+        } else {
+
+          document.getElementById(e).classList.add('is-done');
+
+        }
+
+      }
+
+
+
+
+     add() {
+
+        var ul = document.getElementById('tasks');
+
+        var li = document.createElement('li');
+
+      
+
+        var nextid = document.querySelectorAll('ul li').length + 1;
+
+      
+
+        if (document.getElementById('item').value.length > 0) {
+
+          li.appendChild(
+
+            document.createTextNode(document.getElementById('item').value)
+
+          );
+
+          li.setAttribute('id', nextid);
+
+          li.setAttribute('onclick', 'changeclass(' + nextid + ')');
+
+          ul.appendChild(li);
+
+        }
+
+      }
+
+
+
+
+  render() {
+
+    return (
+
+      <>
+
+        <div>
+
+          <h2>My List</h2>
+
+          <input type="text" id="item" name="item" />
+
+           
+
+          <button onClick={this.add}>Add</button>
+
+          <ul id="tasks"></ul>
+
+        </div>
+
+
+
+
+        <style>{`
+
+                    .is-done {
+
+                        text-decoration: line-through;
+
+                    }
+
+                `}</style>
+
+      </>
+
+    );
+
+  }
+
 }
+  
